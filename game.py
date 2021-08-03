0c41be3700aacc965a32fb11e5d6d6b51a9ecad2
eat-sleep-code <info@eat-sleep-code.com>
2021-08-03 13:38:11 -0700

    Continue work on in-game display

--- game.py
+++ game.py
@@ -3,6 +3,9 @@ import os
 import pygame
 import time
 from data import Data
+from utils import Image as imageUtils
+from PIL import Image
+
 
 class ViewGame():
 	def __init__(self):
@@ -22,14 +25,41 @@ class ViewGame():
 		
 # ---------------------------------------------------------------------
 
+		cellPadding = 10	
+		columnLeftStart = 20
+		columnRightStart = 400
+		itemY = 20
 		refreshCount = 0
-		while globals.gameInProgress == True:	
-			current = Data.getCurrentPlay(globals.gameLink)   
+		thumbnailWidth = 64
+		thumbnailHeight = 64
 
-			pygame.display.set_caption(globals.title + ' > ' + current.away.name + ' @ ' + current.home.name)
+		while globals.gameInProgress == True:
+			# AWAY SCORE	
+			item = Data.getCurrentPlay(globals.gameLink)   
+
+			pygame.display.set_caption(globals.title + ' > ' + item.away.name + ' @ ' + item.home.name)
 		
+			awayLogoPath = imageUtils.webImage(item.away.logo)
+			awayLogo = pygame.image.load(awayLogoPath)
+			awayLogo = pygame.transform.smoothscale(awayLogo, (thumbnailWidth - (cellPadding*2), thumbnailHeight - (cellPadding*2)))
+			globals.displaySurface.blit(awayLogo, (columnLeftStart, itemY + 20))
+		
+			awayScoreText = scoreFont.render(str(item.away.runs), True, (0, 0, 0))
+			globals.displaySurface.blit(awayScoreText, (columnLeftStart, itemY + thumbnailHeight + 20 ))
+
+
+			# HOME SCORE
+
+			homeLogoPath = imageUtils.webImage(item.home.logo)
+			homeLogo = pygame.image.load(homeLogoPath)
+			homeLogo = pygame.transform.smoothscale(homeLogo, (thumbnailWidth - (cellPadding*2), thumbnailHeight - (cellPadding*2)))
+			globals.displaySurface.blit(homeLogo, (columnRightStart, itemY + 20))
+	
+			homeScoreText = scoreFont.render(str(item.home.runs), True, (0, 0, 0))
+			globals.displaySurface.blit(homeScoreText, (columnRightStart, itemY + thumbnailHeight + 20 ))
+					
 			
-			print(current.status.description)
+			print(item.status.description)
 
 			pygame.display.flip()
 			refreshCount += 1
